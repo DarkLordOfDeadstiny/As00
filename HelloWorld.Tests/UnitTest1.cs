b200dac3742a553af37b7147e1fc5c79d4352045
Patrick <43612965+DarkLordOfDeadstiny@users.noreply.github.com>
2021-09-06 17:19:34 +0200

    Is leep year (#2)
    * IsLeapYear is implemented with interface and tested

--- HelloWorld.Tests/UnitTest1.cs
+++ HelloWorld.Tests/UnitTest1.cs
@@ -1,14 +1,36 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace HelloWorld.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(4, true)]
+        [InlineData(100, false)]
+        [InlineData(200, false)]
+        [InlineData(400, true)]
+        [InlineData(1800, false)]
+        [InlineData(1600, true)]
+        public void LeapYear_check(int year, bool expected)
         {
-
+            Assert.Equal(expected, Program.IsLeapYear(year));
         }
+
+        [Fact]
+        public void Main_prints_yay()
+        {
+            var writter = new StringWriter();
+            Console.SetOut(writter);
+
+            Program.Main(new string[]{"2020"});
+
+
+            Assert.Equal("yay", writter.GetStringBuilder().ToString().Trim());
+        }
+
+
+
     }
 }
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,11 +2,57 @@
 
 namespace HelloWorld
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int indput;
+            if (args != null && args.Length != 1)
+            {
+                Console.WriteLine("Enter a year after 1582 to check if it is a leap year :)");
+                indput = getIndput();
+            }
+            else
+            {
+                indput = Convert.ToInt32(args[0]);
+            }
+            
+            if (indput>=1582)
+            {
+                printAnswer(IsLeapYear(indput));
+            }
+        }
+
+        static int getIndput()
+        {
+            try
+            {
+                return Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("try again with a year as a real number larger than 1582");
+                return getIndput();
+            }
+        }
+
+        static void printAnswer(bool isLeapYear)
+        {
+            if (isLeapYear)
+            {
+                Console.WriteLine("yay");
+                return;
+            }
+            Console.WriteLine("nay");
+        }
+
+        public static bool IsLeapYear(int year)
+        {
+            if ((year%4 == 0 && year%100 != 0) || year % 400 == 0)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
